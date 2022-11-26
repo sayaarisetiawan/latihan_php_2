f05c28ea86c5f27389319eb5baaabe0fa7de070c
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-26 22:53:07 +0700

    Task 9

--- latihan2.php
+++ latihan2.php
@@ -1,13 +1,10 @@
 <?php
-echo "Apakah 10 kurang dari 20<br>";
-echo "<br>";
-     $x =10;
-     $t = "Ya";
-     $f = "Tidak";
-
-     if ($x < 20) {
-        echo "$t";
-     } else {
-        echo "$f";
-     }
+$x =20;
+if ($x < 10) {
+    echo "benar";
+} elseif ($x == 20) {
+    echo "BINGGO";
+} else {
+    echo "salah";
+}
 ?>
