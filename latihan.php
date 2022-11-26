6c0b845fd3e9f1b34b1d6f3ae73f7e22fdc50675
sayaarisetiawan <ari_22510010@stimata.ac.id>
2022-11-26 22:22:50 +0700

    Task 5

--- latihan.php
+++ latihan.php
@@ -1,9 +1,20 @@
 <?php
-echo "Menggunakan perulangan While<br>";
-echo "<br>";
-$i = 1;
-while ($i <= 5) {
-    echo "Hai, ini baris ke-$i<br>";
+// perulangan
+// do while
+// echo "<h2>Menggunakan perulangan Do While</h2>";
+// $i= 10;
+// do {
+//     echo "Hello World!<br>";
+//     $i++;
+// } while ($i < 5); 
+// nilai incremet(5) lebih kecil dari inisialisasi variabel i (10) yang artinya kondisi false,
+// teks ditampilkan satu baris karena perulangan do while menampilan perintah minimal sekali jika kondisi
+// bernilai false, berikut nilai imput yang benar agar program baris ditampilkan sebanyak 5 baris.
+echo "<h2>Menggunakan perulangan Do While</h2>";
+$i= 1;
+do {
+    echo "Hello World!<br>";
     $i++;
-}
+} while ($i <= 5);
+
 ?>
